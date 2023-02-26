250d97235a7e374c3ec0da2cc9571a4f420a5757
bunyodfcm <bunyod5877@gmail.com>
2023-02-26 19:47:01 +0500

    first

---
+++ src/companent/home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="home-wrapper">
+          <div className="home-logo">
+            <span className="home-logo__t">t</span>
+            <span className="home-logo-o">o</span>
+            <span className="home-logo-d">d</span>
+            <span className="home-logo-oo">o</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
